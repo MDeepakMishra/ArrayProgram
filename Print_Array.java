2f74f8524f1a3423e8155d56d7914cbd324dd9fa
rahul <rahulkumar8826234298@gmail.com>
2025-03-12 13:11:23 +0530

    Initial Commit

---
+++ Print_Array.java
@@ -0,0 +1,9 @@
+class ArrayProgram{
+  public static void main(String[] args) {
+        System.out.println("PRINT THE ARRAYS");
+        int n = 10 ;
+        for(int i = 1 ; i <= n ; i++){
+          System.out.print(i + " ");
+        }
+    }
+}
