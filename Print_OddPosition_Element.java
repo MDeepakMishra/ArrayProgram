405f5e40463b4b3b794b40562e619fe87e9cca73
deep misra <deepakmishra93152@gmail.com>
2025-03-17 21:35:32 +0530

    Print Odd Position Number

---
+++ Print_OddPosition_Element.java
@@ -0,0 +1,15 @@
+public class Print_OddPosition_Element {
+    
+    public static void main(String[] args) {
+        System.out.println("PRINT ODD POSITTION ARRAY ELEMENTS");
+        int a [] = { 1 , 4, 9 , 8 , 7 , 2};
+        for(int i = 0 ; i < a.length ; i++)
+        {
+            if( i % 2 ==0)
+            {
+                System.out.println("INDEXNUMBER" + " " +  i + " " + "ARRAY ELEMENT" + " " + a[i]);
+            }
+        }
+    }
+    
+}
