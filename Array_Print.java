33f74f0428b99dcbcbbef16e3086c62e9df13e33
deep misra <deepakmishra93152@gmail.com>
2025-03-17 19:24:00 +0530

    MY FIRST ARRAY PROGRAM

---
+++ Array_Print.java
@@ -0,0 +1,13 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class Array_Print {
+
+    public static void main(String[] var0) {
+       int[] var1 = new int[]{1, 2, 3, 4};
+ 
+       for(int var2 = 0; var2 < var1.length; ++var2) {
+          System.out.print(var1[var2] + " ");
+       }
+ 
+    }
+ }
+ 
